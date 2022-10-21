bcf8fcb8aede4bbd6e3741e5a853b3032260aa85
leejohy-0223 <leejohy@naver.com>
2022-10-21 23:58:24 +0900

    feat: Config 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,7 +2,11 @@ package hello.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import hello.proxy.config.AppV1Config;
+
+@Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 

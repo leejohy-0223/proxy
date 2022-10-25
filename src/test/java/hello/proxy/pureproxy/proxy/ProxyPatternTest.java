3734288734edc567b9fbddac8e9fcee25e1ea330
leejohy-0223 <leejohy@naver.com>
2022-10-25 19:58:35 +0900

    feat: proxy 패턴 적용 전

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,18 @@
+package hello.proxy.pureproxy.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
